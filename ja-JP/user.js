82a4f1a148daabc994707d121cea58d9d6253223
Jan Dvorak <storytellercz@protonmail.com>
2021-11-14 16:45:22 +0100

    New translations user.js (Japanese)

---
+++ ja-JP/user.js
@@ -0,0 +1,30 @@
+export default {
+    'user.friend.request.add': 'Add to friends',
+    'user.friend.requests': `You have {num, number} new friend {num, plural,
+    zero {requests}
+    one {request}
+    many {requests}
+  }`,
+    'user.friend.unfriend': 'Unfriend',
+    'user.block': 'Block',
+    'user.unblock': 'Unblock',
+    'user.joined': `{gender, select,
+    male {Joined}
+    female {Joined}
+    other {Joined}} on {date, date}`,
+    'user.friend.request.accept': 'Accept friendship request',
+    'user.friend.request.cancel': 'Cancel friendship request',
+    'user.friend.request.deny': 'Deny friendship',
+    'user.listing': 'User listing',
+    'user.profile.visit': 'Visit {user}\'s profile.',
+    'user.profile.avatar': '{username}\'s avatar',
+    'user.friend.list.requests': 'Friendships requests',
+    'user.friend.norequests': 'You have no requests for friendship.',
+    'user.dashboardWelcome': `Good {dayPart, select,
+    morning {morning}
+    afternoon {afternoon}
+    evening {evening}
+    night {night}
+    other {day}
+  } {displayName}!`
+};
