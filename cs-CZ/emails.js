1473c30dd598a99ac8a62a1e2e584a015aab87f4
Jan Dvorak <storytellercz@protonmail.com>
2021-12-12 10:19:49 +0100

    New translations emails.js (Czech)

--- cs-CZ/emails.js
+++ cs-CZ/emails.js
@@ -7,27 +7,27 @@ export default {
     'email.enrollAccount.message': `Hello {username}\n\n
     We would like to invite you to Socialize-starter! We have created a special personal account for you where you can access all features of Socialize-starter!
     To activate your account, simply click the link below:\n\n{url}`,
-    'email.resetPassword.subject': 'Socialize-starter reset password',
+    'email.resetPassword.subject': 'Socialize-starter obnovení hesla',
     'email.resetPassword.message': `Hello {username}!\n\n We have received a request to reset your password for your account.
         Please follow the link bellow to reset your password:\n\n{url}`,
-    'email.verifyEmail.subject': 'Socialize-starter e-mail verification',
+    'email.verifyEmail.subject': 'Socialize-starter ověření e-mailu',
     'email.verifyEmail.message': 'Hello {username}!\n\n Please verify your e-mail address by clicking on the link below:\n\n{url}',
     // New html email keys
     'email.salutation': 'Dobrý den {username},',
     'email.welcome': 'Vítejte v Socialize-startéru!',
-    'email.signature': 'Sincerely,<br />Socialize-starter team',
+    'email.signature': 'S pozdravem,<br />Socialize-starter tým',
     // Enrollment
     'email.enrollAccount.welcome': 'Vítejte na palubě',
     'email.enrollAccount.messageHtml': 'Chtěli bychom vás pozvat do socializace startu! Šli jsme vpřed a připravili jsme pro Vás osobní účet, kde můžete přistupovat ke všem funkcím socialize-starteru!',
     'email.enrollAccount.button': 'Zpřístupnit účet',
-    'email.enrollAccount.signature': 'We are looking forward to see you,<br />Socialize-starter team',
+    'email.enrollAccount.signature': 'Těšíme se na Vás,<br />Socialize-starter tým',
     // Reset password
     'email.resetPassword.messageHtml': 'Obdrželi jsme Váš požadavek na resetování Vašeho hesla, prosím klikněte na tlačítko níže k dokončení tohoto procesu.',
     'email.resetPassword.button': 'Resetovat heslo',
     // E-mail verification
     'email.verifyEmail.messageHtml': 'Vítejte na palubě! Pro potvrzení této e-mailové adresy prosím klikněte na tlačítko níže.',
     'email.verifyEmail.button': 'Potvrzení e-mailové adresy',
-    'email.verification': 'E-mail verification',
-    'email.verifying': 'Verifying your e-mail. This will take just a moment...',
-    'email.verified': 'Great news! Your e-mail has been verified!'
+    'email.verification': 'Ověření emailu',
+    'email.verifying': 'Ověřování Vašeho e-mailu. Tohle může chvilku trvat...',
+    'email.verified': 'Skvělé zprávy! Váš e-mail byl ověřen!'
 }    /* eslint-enable max-len */;
